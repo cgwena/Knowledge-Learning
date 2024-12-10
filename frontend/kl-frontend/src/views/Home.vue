61b2ad9beef5629e678a085c6ebbf60a606ab53b
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-12-10 16:41:50 +0100

    Front-end Vuejs added

---
+++ frontend/kl-frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <img alt="Knowledge logo" src="../assets/Logo_KL.png">
+    <h1>Bienvenue chez KnowledgeLearning</h1>
+    <p>Veuillez vous connecter pour accéder à votre compte</p>
+    <LoginForm />
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+
+export default {
+    name: 'HomePage',
+    components: {
+   LoginForm
+  }
+}
+
+</script>
+
+<style scoped></style>
